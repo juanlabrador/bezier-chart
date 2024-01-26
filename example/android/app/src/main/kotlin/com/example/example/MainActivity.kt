7ad364ea9ea7e744168faaab3ba4446a96ba9c37
Juan Labrador <156969154+juan-labrador@users.noreply.github.com>
2024-01-26 10:29:26 -0500

    update

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.example
 
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
+
 }
